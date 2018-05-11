078be466406b2d87de1b5eb5e22b86d56f11d912
Pushpe <pushpakumara.14@cse.mrt.ac.lk>
2018-05-11 17:42:21 +0530

    initial commit

---
+++ Test.php
@@ -0,0 +1,49 @@
+<?php
+$serverName = "cryptolive.database.windows.net";
+$connectionOptions = array(
+    "Database" => "cryptopivot",
+    "Uid" => "kierancrypto",
+    "PWD" => "2Sugarsplease"
+);
+
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if( $conn === false ) {
+    die( FormatErrors( sqlsrv_errors()));
+}
+
+//Select Query
+$tsql= "SELECT @@Version as SQL_VERSION";
+
+//Executes the query
+$getResults= sqlsrv_query($conn, $tsql);
+
+//Error handling
+if ($getResults == FALSE)
+    die(FormatErrors(sqlsrv_errors()));
+echo "Hello";
+
+?>
+
+<h1> Results : </h1>
+
+<?php
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo ($row['SQL_VERSION']);
+    echo ("<br/>");
+}
+
+sqlsrv_free_stmt($getResults);
+
+function FormatErrors( $errors )
+{
+    /* Display errors. */
+    echo "Error information: <br/>";
+    foreach ( $errors as $error )
+    {
+        echo "SQLSTATE: ".$error['SQLSTATE']."<br/>";
+        echo "Code: ".$error['code']."<br/>";
+        echo "Message: ".$error['message']."<br/>";
+    }
+}
+?>
